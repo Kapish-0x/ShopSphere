facc61bc7fe511a7aa5331f766b98ee5e1ac4cc4
Kapish Rawat <kapishsinghrawat@gmail.com>
2026-09-24 16:23:38 +0530

    Fix

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://shopsphere-4c0v.onrender.com";
+const BASE_URL = "https://shopsphere-4c0v.onrender.com/api";
 
 const axiosInstance = axios.create({ 
   baseURL: BASE_URL,
